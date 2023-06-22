6adc5e01250c7c66c9c0fbddfdab4e00a47cbb07
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-06-22 12:27:25 -0400

    sort of complete bitbuffer, begin writing tests

--- src/net_messages.zig
+++ src/net_messages.zig
@@ -39,7 +39,7 @@ fn processControlMessage(cmd: i32, buf: []u8) !bool {
 }
 
 pub const SimpleBuffer = struct {
-    data: []u8,
+    data: []const u8,
     head: u32,
 
     pub fn processMessages(self: @This()) void {
@@ -64,7 +64,7 @@ pub const SimpleBuffer = struct {
         }
 
         // get an array of bytes which include all the bits we want to return
-        const bytes = std.math.ceil(bits / 8);
+        const bytes: usize = @floatToInt(usize, std.math.ceil(@intToFloat(f32, bits) / 8.0));
         const selection = block: {
             const head_slice = self.slice();
             if (bytes > head_slice.len) {
@@ -72,7 +72,37 @@ pub const SimpleBuffer = struct {
             }
             break :block head_slice[0..bytes];
         };
-        _ = selection;
+
+        // the output where we will store the read bits
+        var result: u32 = 0;
+
+        // the head pointer but it's been shifted back to the beginning of its byte
+        var local_head: u32 = (selection.ptr - self.data.ptr) * 8;
+        const head_bit_from_byte_offset: u8 = local_head - self.head;
+        for (selection, 0..) |byte, byteindex| {
+            // loop through all the bits in this byte
+            for ([_]u8{ 0, 1, 2, 3, 4, 5, 6, 7 }) |bitindex| {
+                // skip the bit if its before the read head
+                // FIXME: this is prone to off-by-one errors and defeats the
+                // purpose of range-based for loops
+                if (local_head < self.head or local_head >= self.head + bits) {
+                    local_head += 1;
+                    continue;
+                }
+
+                // otherwise mask out this bit and apply it to the result
+                const mask: u8 = (@intCast(u8, 1) << bitindex);
+                const bit_in_byte: u8 = mask & byte;
+                // could quit here if bit_in_byte is zero...
+                const bit_in_long: u32 = bit_in_byte << ((byteindex * 8) - head_bit_from_byte_offset);
+                result &= bit_in_long;
+
+                local_head += 1;
+            }
+        }
+
+        self.moveHeadBy(bits);
+        return result;
     }
 
     /// Returns a slice of the array from the byte the head is in until data end
@@ -82,11 +112,11 @@ pub const SimpleBuffer = struct {
     }
 
     fn getRemainingBits(self: @This()) u32 {
-        return self.data.len - self.head;
+        return (self.data.len * 8) - self.head;
     }
 
     fn moveHeadBy(self: @This(), increment: u32) void {
-        std.debug.assert(self.head + increment < self.data.len);
+        std.debug.assert(std.math.ceil((self.head + increment) / 8) < self.data.len);
         self.head += increment;
     }
 };
@@ -125,4 +155,25 @@ fn dWordSwap(val: anytype) @TypeOf(val) {
     temp |= ((*(@ptrCast(*u32, &val)) & 0x000000FF) << 24);
 
     return *@ptrCast(*@TypeOf(val), &temp);
+}
+
+test "SimpleBufferBitTest" {
+    const data: u32 = 0b10101010100011111;
+    var bitbuf: SimpleBuffer = .{
+        .data = block: {
+            var result: []const u8 = undefined;
+            result.ptr = @ptrCast([*]const u8, &data);
+            result.len = @sizeOf(u32);
+            break :block result;
+        },
+        .head = 0,
+    };
+    const first_3_bits = bitbuf.readBits(3);
+    const next_4_bits = bitbuf.readBits(4);
+    const next_byte = bitbuf.readBits(8);
+    const last_two_bits = bitbuf.readBits(2);
+    try std.testing.expectEqual(first_3_bits, @intCast(u32, 0b0111));
+    try std.testing.expectEqual(next_4_bits, @intCast(u32, 0b0011));
+    try std.testing.expectEqual(next_byte, @intCast(u32, 0b10101010));
+    try std.testing.expectEqual(last_two_bits, @intCast(u32, 0b10));
 }
